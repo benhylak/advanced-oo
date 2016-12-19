30c35f7d3868824e3992fc3992642889177f6ecd
bhylak <blhylak@wpi.edu>
2016-12-18 19:06:16 -0500

    Removed extraneous files

---
+++ Project4/src/Carson.java
@@ -0,0 +1,12 @@
+/**
+ * Even Carson isn't quite sure why he's been included here.
+ */
+public class Carson extends Animal
+{
+    static String imagePath = "Resources/Pictures/carson.png";
+
+    public Carson(int xPos, int yPos)
+    {
+        super(xPos, yPos, imagePath);
+    }
+}
