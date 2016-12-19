576af5c058723cb5cf3a632efc74aaf332b05db7
bhylak <blhylak@wpi.edu>
2016-12-18 19:06:15 -0500

    Logic all transferred to environment class. Hit detection with paddle and ball and corresponding events working.

--- Project4/src/Ball.java
+++ Project4/src/Ball.java
@@ -1,8 +1,12 @@
-import java.awt.*;
+import java.util.ArrayList;
+import java.util.Collection;
 
+import javafx.geometry.BoundingBox;
+import javafx.geometry.Bounds;
 import javafx.scene.Node;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 /**
  * Class that implements a ball with a position and velocity.
@@ -26,8 +30,11 @@ public class Ball extends GameObj implements Drawable
 	// Instance variables
 	// (x,y) is the position of the center of the ball.
 	private double x, y;
-	private double vx, vy;
 	private Circle circle;
+
+	private ArrayList<WallCollisionEvent.WallCollisionListener> collisionListeners;
+	private ArrayList<AnimalHitEvent.AnimalHitListener> hitListeners;
+
 
 	public Node getDrawing()
 	{
@@ -45,16 +52,20 @@ public class Ball extends GameObj implements Drawable
 	 * Constructs a new Ball object at the centroid of the game board
 	 * with a default velocity that points down and right.
 	 */
-	public Ball () {
+	public Ball ()
+	{
 		x = GameImpl.WIDTH/2;
 		y = GameImpl.HEIGHT/2;
-		vx = INITIAL_VX;
-		vy = INITIAL_VY;
+		xVel = INITIAL_VX;
+		yVel = INITIAL_VY;
 
 		circle = new Circle(BALL_RADIUS, BALL_RADIUS, BALL_RADIUS);
 		circle.setLayoutX(x - BALL_RADIUS);
 		circle.setLayoutY(y - BALL_RADIUS);
 		circle.setFill(Color.BLACK);
+
+		collisionListeners = new ArrayList<>();
+		hitListeners = new ArrayList<>();
 	}
 
 	/**
@@ -62,13 +73,80 @@ public class Ball extends GameObj implements Drawable
 	 * based on the specified elapsed time since the last update.
 	 * @param deltaNanoTime the number of nanoseconds that have transpired since the last update
 	 */
-	public void updatePosition (long deltaNanoTime) {
-		double dx = vx * deltaNanoTime;
-		double dy = vy * deltaNanoTime;
+	public void updatePosition (long deltaNanoTime)
+	{
+		double dx = xVel * deltaNanoTime;
+		double dy = yVel * deltaNanoTime;
+
 		x += dx;
 		y += dy;
 
 		circle.setTranslateX(x - (circle.getLayoutX() + BALL_RADIUS));
 		circle.setTranslateY(y - (circle.getLayoutY() + BALL_RADIUS));
+	}
+
+//	private BoundingBox.BoundingPoint getLowerLeft()
+//	{
+//		return new BoundingBox.BoundingPoint(circle.getTranslateX() + circle.getLayoutX(),
+//				circle.getTranslateY() + circle.getLayoutY());
+//	}
+
+	public void checkCollisions(Collection<GameObj> objs)
+	{
+		for(GameObj obj : objs)
+		{
+			if(obj.isCollision(this))
+			{
+				if(obj instanceof Wall)
+				{
+					notifyWallCollision((Wall)obj);
+				}
+				else if(obj instanceof Animal)
+				{
+					notifyAnimalHit((Animal)obj);
+				}
+				else if(!(obj instanceof Ball))
+				{
+					System.out.println("Collided with Paddle");
+				}
+			}
+		}
+	}
+
+	public void addWallCollisionListener(WallCollisionEvent.WallCollisionListener listener)
+	{
+		collisionListeners.add(listener);
+	}
+
+	public void addAnimalHitListener(AnimalHitEvent.AnimalHitListener listener)
+	{
+		hitListeners.add(listener);
+	}
+
+
+	public void notifyWallCollision(Wall wall)
+	{
+		for(WallCollisionEvent.WallCollisionListener l : collisionListeners)
+		{
+			l.handleCollision(new WallCollisionEvent(wall, this));
+		}
+	}
+
+	public void notifyAnimalHit(Animal a)
+	{
+		for(AnimalHitEvent.AnimalHitListener l : hitListeners)
+		{
+			l.handleAnimalHit(new AnimalHitEvent(a, this));
+		}
+	}
+
+	@Override
+	public Bounds getBoundingBox()
+	{
+		//BoundingBox.BoundingPoint lowerLeft = getLowerLeft();
+
+		//BoundingBox.BoundingPoint upperRight = new BoundingBox.BoundingPoint(lowerLeft, BALL_RADIUS, BALL_RADIUS);
+
+		return circle.getBoundsInParent();
 	}
 }
--- Project4/src/GameImpl.java
+++ Project4/src/GameImpl.java
@@ -5,7 +5,8 @@ import javafx.scene.input.MouseEvent;
 import javafx.event.*;
 import java.util.*;
 
-public class GameImpl extends Pane implements Game {
+public class GameImpl extends Pane implements Game
+{
 
 
 	// Constants
@@ -23,81 +24,102 @@ public class GameImpl extends Pane implements Game {
 	private Paddle paddle;
 	Environment env;
 
+	Label startLabel;
+
 	/**
 	 * Constructs a new GameImpl.
 	 */
-	public GameImpl () {
+	public GameImpl()
+	{
 		setStyle("-fx-background-color: white;");
 		env = new Environment(WIDTH, HEIGHT);
 
-		env.addStateChangeListener( () ->
+		env.addStateChangeListener(e ->
 		{
-			getChildren().clear();  // remove all components from the game
-			getChildren().addAll(env.getDrawings());
-        });
-
-
-
-		restartGame(Environment.GameState.NEW);
-	}
-
-	public String getName () {
-		return "Zutopia";
-	}
-
-	public Pane getPane () {
-		return this;
-	}
-
-	private void restartGame (Environment.GameState state) {
-
-		// Add start message
-		final String message;
-
-
-		final Label startLabel = new Label(message + "Click mouse to start");
-		startLabel.setLayoutX(WIDTH / 2 - 50);
-		startLabel.setLayoutY(HEIGHT / 2 + 100);
-		getChildren().add(startLabel);
-
-		// Add event handler to start the game
-		setOnMouseClicked(new EventHandler<MouseEvent> () {
-			@Override
-			public void handle (MouseEvent e) {
-				GameImpl.this.setOnMouseClicked(null);
-
-				// As soon as the mouse is clicked, remove the startLabel from the game board
-				getChildren().remove(startLabel);
+			if(e.getState() == Environment.GameState.ACTIVE)
+			{
 				run();
+			}
+			else
+			{
+				getChildren().clear();  // remove all components from the game
+				getChildren().addAll(env.getDrawings());
+
+				startLabel = new Label(env.getMessage() + "Click mouse to start");
+
+				startLabel.setLayoutX(WIDTH / 2 - 50);
+				startLabel.setLayoutY(HEIGHT / 2 + 100);
+
+				getChildren().add(startLabel);
 			}
 		});
 
-		// Add another event handler to steer paddle...
+		setOnMouseMoved(event ->
+				{
+					if (env.isActive())
+					{
+						env.movePaddleTo(event.getX(), event.getY());
+					}
+				}
+		);
+
+		setOnMouseClicked(new EventHandler<MouseEvent>()
+		{
+			@Override
+			public void handle(MouseEvent e)
+			{
+				//GameImpl.this.setOnMouseClicked(null);
+				// As soon as the mouse is clicked, remove the startLabel from the game board
+				getChildren().remove(startLabel);
+				env.startGame();
+			}
+		});
+
+		env.newGame();
 	}
+
+	public String getName()
+	{
+		return "Zutopia";
+	}
+
+	public Pane getPane()
+	{
+		return this;
+	}
+
 
 	/**
 	 * Begins the game-play by creating and starting an AnimationTimer.
 	 */
-	public void run () {
+	public void run()
+	{
 		// Instantiate and start an AnimationTimer to update the component of the game.
-		new AnimationTimer () {
+		AnimationTimer a = new AnimationTimer()
+		{
 			private long lastNanoTime = -1;
-			public void handle (long currentNanoTime) {
-				if (lastNanoTime >= 0) {  // Necessary for first clock-tick.
-					GameState state;
-					if ((state = runOneTimestep(currentNanoTime - lastNanoTime)) != GameState.ACTIVE) {
-						// Once the game is no longer ACTIVE, stop the AnimationTimer.
-						stop();
-						// Restart the game, with a message that depends on whether
-						// the user won or lost the game.
-						restartGame(state);
-					}
+
+			public void handle(long currentNanoTime)
+			{
+				if (lastNanoTime >= 0)
+				{
+					env.runTimeStep(currentNanoTime-lastNanoTime);
 				}
 				// Keep track of how much time actually transpired since the last clock-tick.
 				lastNanoTime = currentNanoTime;
 			}
-		}.start();
+		};
+
+		env.addStateChangeListener(e -> {
+			if(e.getState() != Environment.GameState.ACTIVE)
+			{
+				a.stop();
+			}
+		});
+
+		a.start();
 	}
+}
 
 	/**
 	 * Updates the state of the game at each timestep. In particular, this method should
@@ -105,8 +127,4 @@ public class GameImpl extends Pane implements Game {
 	 * @param deltaNanoTime how much time (in nanoseconds) has transpired since the last update
 	 * @return the current game state
 	 */
-	public GameState runOneTimestep (long deltaNanoTime) {
-		ball.updatePosition(deltaNanoTime);
-		return GameState.ACTIVE;
-	}
-}
+
